d43091cbe3bc75675b7ee37a514a3a08e0c1d18a
SJGadmin <grant@stewartandjane.com>
2025-08-20 10:29:59 -0500

    Create TypingDots.tsx

---
+++ components/TypingDots.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function TypingDots() {
+  return (
+    <div className="typing">
+      <span className="dot" />
+      <span className="dot" />
+      <span className="dot" />
+    </div>
+  );
+}
